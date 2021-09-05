1a28d4b202882df12b9ea9ca3b9274bf68ee245a
Samuel de oliveira <samuel.deoliveir1@gmail.com>
2021-09-05 17:00:58 +0200

    début route

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../vendor/autoload.php';
+$router = new AltoRouter();
+$router->map('GET', '/', function () {
+    require __DIR__ . '/../view/home.php';
+});
+$router->map('GET', '/home', function () {
+    require __DIR__ . '/../view/home.php';
+});
+$router->map('GET', '/admin', function() {
+    require __DIR__ . '/../view/admin.php';
+});
+$router->map('GET', '/contact', function () {
+    require __DIR__ . '/../view/contact.php';
+});
+
+$match = $router->match();
+
+if (is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // no route was matched
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
